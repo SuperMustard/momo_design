cb1e00dc422eb651402fb1461e6ec02731eae9e0
SuperMustard <mr.jiahanxin@gmail.com>
2024-11-20 10:58:14 -0500

    add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 
 function App() {
   return (
@@ -7,6 +8,32 @@ function App() {
         <h1>Hello world</h1>
         <h2>Hello Java</h2>
         <code>const a = 'b'</code>
+        <Button
+          autoFocus
+          onClick={(e) => {
+            alert("kneel down!");
+          }}
+          className="TestCustom"
+        >
+          Hello
+        </Button>
+        <Button btnType={ButtonType.Default} size={ButtonSize.Large}>
+          Mistress Xuan
+        </Button>
+        <Button btnType={ButtonType.Default} size={ButtonSize.Large} disabled>
+          Mistress Xuan
+        </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small} disabled>
+          Mistress Xuan
+        </Button>
+        <Button btnType={ButtonType.Danger}>Mistress Baixue</Button>
+        <Button
+          btnType={ButtonType.Link}
+          href="http://wwww.google.com"
+          size={ButtonSize.Large}
+        >
+          Mistress Xue
+        </Button>
       </header>
     </div>
   );
